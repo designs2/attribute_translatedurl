b5a6e6c148a447f3635d782b5d46655901823578
cboelter <christopher@boelter.eu>
2014-12-20 17:04:53 +0100

    added missing languages

---
+++ contao/languages/en/tl_metamodel_attribute.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package    MetaModels
+ * @subpackage AttributeUrl
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Andreas Isaak <info@andreas-isaak.de>
+ * @author     Christopher Boelter <christopher@boelter.eu>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedurl']    = 'Translated-URL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['trim_title']            = array('Remove title', 'If selected, only the link field is used.');
