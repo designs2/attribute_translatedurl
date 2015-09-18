930811b447c6a35d2f5da85cfb46ebf1d3bd8044
Mini Model <minimodel@metamodel.me>
2015-09-18 10:35:06 +0200

    Updated translations from transifex.

---
+++ contao/languages/rm/tl_metamodel_attribute.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T08:04:02+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['trim_title']['0'] = 'Allontanar il titel';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['trim_title']['1'] = 'Sche quest champ vegn tschernì vegn be il champ da colliaziun utilisà.';
+
